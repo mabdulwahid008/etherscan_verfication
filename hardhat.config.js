fe61cc2539b186225eaa3450fd5908e817d3b3e9
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-02-09 23:38:33 +0500

    installed some dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: process.env.URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
