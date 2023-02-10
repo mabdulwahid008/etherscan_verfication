36430eafb0a2bd821dbca8ab1c8d486607d30bac
mabdulwahid008 <m.abdulwahid008@gmail.com>
2023-02-10 21:39:35 +0500

    commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,14 @@ require('dotenv').config()
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    goerli: {
+    mumbai: {
       url: process.env.URL,
       accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey : {
+      polygonMumbai: process.env.POLYGONSCAN_KEY
     }
   }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,29 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const verifyContract = await ethers.getContractFactory("Verify")
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  const deployeVerfiyContract = await verifyContract.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await deployeVerfiyContract.deployed()
 
-  await lock.deployed();
+  console.log( "Verfiy Contract Address is " + deployeVerfiyContract.address);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("Sleeping ....");
+
+  await sleep(40000)
+
+  await hre.run("verify-verify", {
+    address: deployeVerfiyContract.address,
+    constructorArguments: []
+  })
+
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
